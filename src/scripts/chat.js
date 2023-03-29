8623d5fc08e4571974cab2b8149fe0f9b0836627
Alejandro Muñoz <almulo@gmail.com>
2023-03-29 13:35:47 +0200

    Applying changes

--- src/scripts/chat.js
+++ src/scripts/chat.js
@@ -1,22 +1,27 @@
 let guestChat = document.getElementById("guestChat");
-
+var message ="";
+const messageChat = document.getElementById("messages-chat");
+Hello();
 guestChat.addEventListener("submit", (e) => {
   e.preventDefault();
 
   let guest = document.getElementById("prompt");
 
-  let message = guest.value;
-  
-  const messageChat = document.getElementById("messages-chat");
+  message = guest.value;
 
-  let element = document.getElementById("guestMessage");
-  if(element != null){
-    console.log(1);
-    element.classList.remove("animation-message");
-    element.removeAttribute('id')
+  let elementguest = document.getElementById("guestMessage");
+  let elementgbot = document.getElementById("botMessage");
+  if(elementguest != null){
+    elementguest.classList.remove("animation-message");
+    elementguest.removeAttribute('id')
   }
-  messageChat.innerHTML += `<section class"message"><div id=guestMessage class="guest-message animation-message"><p>${message}</p></div></section>`;
+  if(elementgbot != null){
+    elementgbot.classList.remove("animation-message");
+    elementgbot.removeAttribute('id')
+  }
+  messageChat.innerHTML += `<section class="guest-section"><div id=guestMessage class="guest-message animation-message"><p>${message}</p></div></section>`;
   document.getElementById("guestChat").reset();
+  ComprobarMensaje();
   scrollToBottom();
 });
 
@@ -26,4 +31,29 @@ function scrollToBottom() {
 }
 
 
+///CHAT BOT RESPUESTAS
+const comandos = ["?help", "?juegorandom", "?buscar", "?guardar"];
+function messagebot(messagebot) {
+  return`<section class="bot-section"><div id=botMessage class="bot-message animation-message">${messagebot}</div></section>`;
+}
 
+function Hello(){
+  const Hola = ["Hola en que puedo ayudarte?","En que le puedo servir amoooo...", "Un placer tenerlo por aquí como puedo prestarte servicio"];
+  let rand = Math.floor(Math.random() * 2);
+  messageChat.innerHTML += messagebot("<p>"+Hola[rand]+"</p> <p>?help --> ver comandos</p>");
+  scrollToBottom();
+}
+
+
+// RESPUESTAS
+function ComprobarMensaje(){
+  if(message == "?help"){
+    let listacomandos = ""; 
+    for(let i = 0; i < comandos.length; i++){
+      listacomandos += "<p>"+comandos[i]+"</p>";
+    }
+    messageChat.innerHTML += messagebot("<p>Los comandos son:</p> "+listacomandos);
+  }else{
+    messageChat.innerHTML += messagebot("<p> El mensaje intrducido no es valido prueba con el comando: ?help</p>");
+  }
+}
